e051ff0a2dab83211b6a526e0007ed467b5eecb9
Vander Luis Lauschner <vander.lauschner@proway.treina>
2022-10-19 21:42:26 -0300

    atualiza

---
+++ modulo2/controller.py
@@ -0,0 +1,18 @@
+def salvar(nome):
+    with open('modulo2/pessoas.txt', 'a') as arquivo:
+        arquivo.write(f'{nome}\n')
+        
+def listar():
+    nomes = []
+    with open('modulo2/pessoas.txt', 'r') as arquivo:
+        for name in arquivo:
+            name = name.strip()
+            nomes.append(name)
+    
+    return nomes
+
+salvar('Vander')
+
+#print('Lista de Nomes',listar())
+
+
