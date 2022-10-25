5dafcfccb06a58177ff2440d515b1d3ee834f636
Vander Luis Lauschner <vander.lauschner@proway.treina>
2022-10-24 22:01:39 -0300

    atualiza

--- Exemplos/controller.py
+++ Exemplos/controller.py
@@ -17,3 +17,4 @@ def cadastrar():
       
 def listar():
     print(funcionario)
+    
--- Exemplos/main.py
+++ Exemplos/main.py
@@ -1,4 +1,4 @@
-from controller import *
+from controller import cadastrar, listar
 
 def menu():
     while True:
