94a4c14d554194406537235e300481d6b0c5c07e
Vander Luis Lauschner <vander.lauschner@proway.treina>
2022-11-18 21:55:32 -0300

    atualiza

--- Classes/crud/controller.py
+++ Classes/crud/controller.py
@@ -18,3 +18,36 @@ def read():
         lista_contas.append(conta)
     contas.close
     return lista_contas
+
+def update(conta_update:Conta):
+    lista_contas = []
+    contas = open("contas.txt", "r")  
+    for conta in contas:
+        conta_limpa = conta.strip()
+        conta_objeto = conta_limpa.split(';')
+        
+        if conta_update.numero == int(conta_objeto[1]):
+            lista_contas.append(str(conta_update)+'\n')
+        else:
+            lista_contas.append(conta)
+    contas.close()
+    
+    contas = open("contas.txt", 'w')
+    contas.writelines(lista_contas)
+    contas.close()
+
+def delete(numero_conta):
+    lista_contas = []
+    contas = open("contas.txt", "r")
+    for conta in contas:
+        conta_limpa = conta.strip()
+        conta_objeto = conta_limpa.split(';')
+        if numero_conta != int(conta_objeto[1]):
+            lista_contas.append(str(numero_conta)+'\n')
+        else:
+            lista_contas.append(conta)
+    contas.close()
+    
+    contas = open("contas.txt", 'w')
+    contas.writelines(lista_contas)
+    contas.close()
--- Classes/crud/main.py
+++ Classes/crud/main.py
@@ -1,16 +1,20 @@
 from conta import Conta
-from controller import create, read
+from controller import create, read, update, delete
 
 def menu():
     conta = Conta()
-    conta.titular = 'Vander Lauschner'
-    conta.numero = 456785
-    conta.saldo = 3000.0
+    conta.titular = 'Vander Luis'
+    conta.numero = 123456
+    conta.saldo = 2000.0
     create(conta)
     
-    lista_contas = read()
+    #lista_contas = read()
     
-    for c in lista_contas:
-        print(c)
-        
+    #for c in lista_contas:
+    #    print(c)
+    
+    #delete(85472)
+    
+    #update(85472)
+    
 menu()
