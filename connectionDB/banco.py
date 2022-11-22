773137d0fec8445389b4ed81a2ffff00e4508f09
Vanderlaus <vanderluislauschner@gmail.com>
2022-11-22 11:15:06 -0300

    exercicio conexao banco de dados

---
+++ connectionDB/banco.py
@@ -0,0 +1,18 @@
+import psycopg2
+
+try:
+    conn = psycopg2.connect(host = "localhost", port = "5435", database = "postgres", user="vanderlaus", password = "123456")
+    print("Conexao realizada com sucesso.")
+except:
+    print("Conexao com problema.")
+
+if conn is not None:
+    print("Conexao estavel.")
+
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE teste (id serial PRIMARY KEY, nome VARCHAR(15), sobreNome VARCHAR(15));")
+
+print("Tabela Criada.")
+conn.commit()
+cursor.close()
+conn.close()
