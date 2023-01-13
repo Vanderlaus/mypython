adac2dc9d6bec38f4b8f49ea99ae58f3de69ef4c
Vander Luis Lauschner <vander.lauschner@proway.treina>
2023-01-12 21:51:20 -0300

    subindo exercicio django

--- django/alunos/views.py
+++ django/alunos/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse("<h1>ALUNOS</h1")
+    return render(request,'index.html')
+
+def aluno(request):
+    return render(request,'aluno.html')
 
 # Create your views here.
